c2079c25ac2c456dbac0fc3f409a26750cb45491
eloiseschmitt <eschmitt18@gmail.com>
2021-05-02 10:35:31 +0200

    modif modele Booking + création fichier de migration

--- disquaire_project/store/models.py
+++ disquaire_project/store/models.py
@@ -19,4 +19,4 @@ class Booking(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     contacted = models.BooleanField(default=False)
     contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
-    album = models.OneToOneField(Album)
+    album = models.OneToOneField(Album, on_delete=models.CASCADE)
